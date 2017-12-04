5a2a3445a2ecb013400421a2d03ad9640adec257
Student <Student@10.3.69.40>
2017-12-04 10:39:25 -0800

    For da AI

---
+++ src/Hallo/FallingBullets.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Hallo;
+
+import java.awt.Image;
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import java.util.Random;
+
+/**
+ *
+ * @author Student
+ */
+public class FallingBullets extends Sprite{
+    private int dx;
+    public int x;
+    public int y;
+    private Image image;
+    private final int BOARD_Length = 700;
+    private final int BOARD_WIDTH = 1500;
+    private ArrayList Bullets;
+    Random rng = new Random();
+    private int rngesus = rng.nextInt(1201);
+    public FallingBullets(int x, int y) {
+    super(x, 0);
+        
+        character();
+    }
+private void character() {
+        Bullets = new ArrayList();
+        ImageIcon i = new ImageIcon("octopops.png");
+        image = i.getImage();
+        y = 0;
+        x = rngesus;
+    }
+public void move(int BulletSpeedx, int BulletSpeedy) {
+        
+        x += BulletSpeedx;
+        y += BulletSpeedy;
+        if (x > BOARD_WIDTH) {
+            vis = false;
+        }
+    }
+}
