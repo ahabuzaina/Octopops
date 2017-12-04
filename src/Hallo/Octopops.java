082d229b06ce3228349004bb69ed96f3e9f2ada4
Tranqualizer <Tranqualizer@DESKTOP-E8QHBT8>
2017-12-03 17:21:51 -0800

    Made Octopops move faster

--- src/Hallo/Octopops.java
+++ src/Hallo/Octopops.java
@@ -46,12 +46,15 @@ public class Octopops {
     public void keyPressed(KeyEvent e) {
 
         int key = e.getKeyCode();
-
-        if (key == KeyEvent.VK_A) {
+        if (key == KeyEvent.VK_A && x >= 0) {
+            x = Math.max(0, x);
+            x = Math.min(1400, x);
             dx = -1;
         }
 
-        if (key == KeyEvent.VK_D) {
+        if (key == KeyEvent.VK_D && x <= 1400) {
+            x = Math.max(0, x);
+            x = Math.min(1400, x);
             dx = 1;
         }
 
